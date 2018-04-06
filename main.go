f8201dfaa6d918aa99f00d560f6902b04353f5ec
Sean Brock <sbrock065@gmail.com>
2018-04-06 16:18:13 -0500

    Create main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"time"
+)
+
+// Need to wrap in mainthread...
+func main() {
+	const framerate = 60 * time.Millisecond
+	
+	// Handle Input
+	// Waits for input (key press)
+	/*
+	go func() {
+		for { // window not closed
+			handleInput(getEvent())
+		}
+	}()
+	*/
+	
+	// Rendering loop
+	tick := time.Tick(*frameRate)
+	for { // window not closed
+		<-tick
+		// Clear
+		// Draw
+		// Refresh
+	}	
+}
