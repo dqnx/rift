bd62fd09595ee52fce38ba47a608ce6353546208
Sean <4583759+sean-brock@users.noreply.github.com>
2018-10-26 12:14:15 -0500

    Started refactor

--- main.go
+++ main.go
@@ -1,43 +1,15 @@
 package main
 
-import (
-	"gitlab.com/rauko1753/gorl"
-)
-
-//const debug = true
-const debug = false
-
 func main() {
-
-	// Frametime in microseconds
-	const frameRate = 1000000 / 60
-
-	gorl.TermMustInit()
-	defer gorl.TermDone()
-	running := true
-
-	var scenes SceneManager
-	scenes.Init(&TitleScene{})
-
-	// Handle Input
-	// Waits for input (key press)
-	for running {
-		if scenes.Empty() {
-			running = false
-			break
+	// Launch the game loop
+	update := make(chan bool)
+	gameRunning := true
+	go func() {
+		for gameRunning {
+			<-update
 		}
+	}()
 
-		gorl.TermClear()
+	// Create the window
 
-		// Draw
-		// Row 0 is reserved for status
-		//TermDrawText(0, 0, strconv.FormatFloat(fps, 'f', 1, 64))
-		scenes.RenderAll()
-
-		// Refresh
-		gorl.TermRefresh()
-
-		// Place at end, so first loop will draw immediately.
-		scenes.HandleKeyEvent(gorl.TermGetKey())
-	}
 }
