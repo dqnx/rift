e20973af76ff8d28999fe9d193d79f892ab282e8
Sean <4583759+sean-brock@users.noreply.github.com>
2018-11-16 22:23:56 -0600

    Added tilset.bmp

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"image"
-	"math"
-	"math/rand"
 	"os"
-	"time"
 
 	_ "image/png"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/bmp"
 	"golang.org/x/image/colornames"
 )
 
@@ -20,7 +17,7 @@ func loadPicture(path string) (pixel.Picture, error) {
 		return nil, err
 	}
 	defer file.Close()
-	img, _, err := image.Decode(file)
+	img, err := bmp.Decode(file)
 	if err != nil {
 		return nil, err
 	}
@@ -38,20 +35,20 @@ func run() {
 		panic(err)
 	}
 
-	spritesheet, err := loadPicture("tileset.png")
+	spritesheet, err := loadPicture("tileset.bmp")
 	if err != nil {
 		panic(err)
 	}
 
 	const (
-		spritesheetRows = 3
-		spritesheetCols = 5
-		tileX = 12
-		tileY = 12
+		spritesheetRows = 16
+		spritesheetCols = 16
+		tileX           = 16
+		tileY           = 16
 	)
 
-	var tileSprites [spritesheetCols*spritesheetRows]pixel.Sprite
-	
+	var tileSprites [spritesheetCols * spritesheetRows]*pixel.Sprite
+
 	for x, i := spritesheet.Bounds().Min.X, 0; x < spritesheet.Bounds().Max.X; x += tileX {
 		for y := spritesheet.Bounds().Min.Y; y < spritesheet.Bounds().Max.Y; y += tileY {
 			tileSprites[i] = pixel.NewSprite(spritesheet, pixel.R(x, y, x+tileX, y+tileY))
